49a31436260132185f252bc7fcaeb7b9abc9805c
Antoine Kingue <etorefr@gmail.com>
2023-02-19 16:04:37 +0100

    🎨 improve browser launch to return browser

--- api/src/controllers/browser.ts
+++ api/src/controllers/browser.ts
@@ -1,12 +1,12 @@
-import puppeteer, { Page } from "puppeteer";
+import puppeteer, { Browser, Page } from "puppeteer";
 import Log from "../utils/log";
 
 export default class BrowserController {
   private static prefix = "[browser]";
 
-  public static async launch(): Promise<Page> {
+  public static async launch(): Promise<[Browser, Page]> {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
     });
 
     Log.info(this.prefix, "init browser");
@@ -15,6 +15,6 @@ export default class BrowserController {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     );
 
-    return page;
+    return [browser, page];
   }
 }
