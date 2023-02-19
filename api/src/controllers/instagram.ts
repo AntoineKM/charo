83f3983a78d714624bbbba43806eed5fbe32b104
Antoine Kingue <etorefr@gmail.com>
2023-02-19 16:06:02 +0100

    🎨 improve instagram me to return the user

--- api/src/controllers/instagram.ts
+++ api/src/controllers/instagram.ts
@@ -7,7 +7,7 @@ export default class InstagramController {
     base: "https://instagram.com",
   };
 
-  public static async signIn(token: string) {
+  public static async me(token: string) {
     const cookies = [
       {
         name: "sessionid",
@@ -16,7 +16,7 @@ export default class InstagramController {
       },
     ];
 
-    const page = await BrowserController.launch();
+    const [browser, page] = await BrowserController.launch();
     await page.goto(this.routes.base);
 
     // accept cookies
@@ -24,16 +24,50 @@ export default class InstagramController {
     await page.waitForSelector("div[role='dialog']");
     await page.click("div[role='dialog'] button[tabindex='0']");
     // set cookies
-    Log.info(this.prefix, "set cookies", { ...cookies, value: "**********" });
+    Log.info(this.prefix, "set cookies");
     await page.setCookie(...cookies);
 
     // reload page
     Log.info(this.prefix, "reload page");
     await page.reload();
 
+    const user = await page.evaluate(() => {
+      const scripts = Array.from(
+        document.querySelectorAll("script[type='application/json']")
+      );
+      if (!scripts) throw new Error("user not found");
+      const scheduledServerJS = scripts.find((script) => {
+        const parsedScript = JSON.parse(script.textContent || "");
+        if (parsedScript.require?.[0]?.[0] === "ScheduledServerJS") {
+          return true;
+        }
+        return false;
+      });
+      if (!scheduledServerJS) throw new Error("user not found");
+      const parsedscheduledServerJS = JSON.parse(
+        scheduledServerJS.textContent || ""
+      );
+
+      const XIGSharedData =
+        parsedscheduledServerJS.require?.[0]?.[3]?.[0]?.__bbox.define.find(
+          (a) => a?.[0] === "XIGSharedData"
+        );
+      const XIGSharedDataContent = XIGSharedData?.[2];
+      const parsedXIGSharedDataContentRaw = JSON.parse(
+        XIGSharedDataContent?.raw || ""
+      );
+      const user = parsedXIGSharedDataContentRaw?.config.viewer;
+      return user;
+    });
+    Log.info(this.prefix, "connected as", user.username);
+
+    // TODO: Add the user to the database
+
+    await browser.close();
     try {
       return {
-        username: "anonymous",
+        username: user.username,
+        profilePicture: user.profile_pic_url,
       };
     } catch (error) {
       Log.error(this.prefix, error);
